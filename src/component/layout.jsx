05f281623b99e10055774182c3466023f1f92c74
unknown <gospelkalu33@gmail.com>
2026-05-18 23:48:59 +0100

    nav

--- src/component/layout.jsx
+++ src/component/layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className=" min-h-screen font-sans text-[#2e2c2b] w-full">
       {/* --- DESKTOP SIDE NAVIGATION (STUCK TO RIGHT) --- */}
-      <nav className="fixed bg-white border border-gray-100 py-4 rounded-full backdrop:blur-lg left-1/2 -translate-x-1/2 my-4 shadow-2xl px-9 hidden lg:flex gap-12 text-[11px] tracking-[0.1em] text-[#d84136] z-40 font-bold transition-opacity hover:opacity-100 opacity-90 items-center w-[50%] justify-center">
+      <nav className="fixed bg-white border border-gray-100 py-4 rounded-full backdrop:blur-lg left-1/2 -translate-x-1/2 my-4 shadow-2xl px-9 hidden lg:flex gap-12 text-[11px] tracking-[0.1em] text-[#d84136] z-40 font-bold transition-opacity hover:opacity-100 opacity-90 items-center w-[60%] justify-center mx-auto">
         <a href="/" className="hover:text-[#2e2c2b] transition-colors overflow-y-hidden">
           <img src="/logo-white.jpeg" alt="logo" className="w-12 h-10"/>
         </a>
